0ed6f0918a9a4525f1f4231a9f6e76614d96c58a
kushkamisha <kushka.misha@gmail.com>
2019-02-10 11:06:24 +0200

    Sptil functions to files, cleanup code, add comments

--- controller.js
+++ controller.js
@@ -3,12 +3,13 @@
 const express = require('express')
 const rpn = require('request-promise-native')
 
-const { prettifyCurrencyString } = require('./utils')
+const utils = require('./lib/utils')
+const bitcoin = require('./lib/bitcoin')
 
 const router = express.Router()
 
 router.post('/price', (req, res) => {
-    const currency = prettifyCurrencyString(req.query.currency)
+    const currency = utils.prettifyCurrencyString(req.query.currency)
 
     const baseUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/'
     const url = `${baseUrl}${currency}USD`
@@ -27,6 +28,20 @@ router.post('/price', (req, res) => {
                 message: '<none>'
             })
         })
+})
+
+router.post('/generatePrKey', (req, res) => {
+    const privateKeyBuffer = bitcoin.generatePrKey()
+    const privateKey = privateKeyBuffer.toString('hex')
+
+    res.send({
+        status: 'success',
+        privateKey
+    })
+})
+
+router.post('/toWIF', (req, res) => {
+    // todo
 })
 
 module.exports = router
--- test.js
+++ test.js
@@ -1,42 +1,7 @@
 'use strict'
 
-// https://en.bitcoin.it/wiki/Wallet_import_format
+const bitcoin = require('./lib/bitcoin')
 
-const crypto = require('crypto')
-const bs58 = require('bs58')
-
-// const DH = crypto.createDiffieHellman(256)
-// const privateKey = DH.generateKeys() // buffer type
-
-// console.log(privateKey)
-// console.log('KxFC1jmwwCoACiCAWZ3eXa96mBM6tb3TYzGmf6YwgdGWZgawvrtJ')
-// console.log(privateKey.length)
-
-/**
- * Converts private key from binary to the WIF format
- * @param {Buffer} prkey - Private key
- * @returns {string} Private key in the WIF format
- */
-const prKeyToWIF = prkey => {
-    // Add 0x80 byte to denote mainnet address
-    const prefix = Buffer.from('80', 'hex')
-    prkey = Buffer.concat([prefix, prkey])
-
-    // Perform sha256 hashing two times
-    const hash1 = crypto.createHash('sha256').update(prkey).digest()
-    const hash2 = crypto.createHash('sha256').update(hash1).digest()
-
-    // Get first 4 bytes to create checksum
-    const checksum = hash2.slice(0, 4)
-
-    // Add checksum to the end of private key with prefix
-    prkey = Buffer.concat([prkey, checksum])
-
-    // Convert private key to base58 format
-    return bs58.encode(prkey)
-}
-
-const prkey = 'd44e833ca5b5e0ce40ebe72f586fbc71a357c3bb35403277242570e1c73f13f3'
-// const prkey = '0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D'
-const prkeyBuf = Buffer.from(prkey, 'hex')
-console.log(prKeyToWIF(prkeyBuf))
+const prKey = '60650407ca00eb762bd01233630df3ab4d73625d459d2fbf2c5b6d8b49460f45'
+const prKeyBuffer = Buffer.from(prKey, 'hex')
+console.log(bitcoin.prKeyToWIF(prKeyBuffer))
